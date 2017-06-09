79272ba75941191816da636dac4ebcf3c7e7d3d1
Peter Rønsby <pr@activeforanimals.com>
2017-06-09 14:00:58 +0200

    AFA-115 Removed login form and expanded header width

--- themes/tofu/src/Preprocessor/HeaderMenuPreprocessor.php
+++ themes/tofu/src/Preprocessor/HeaderMenuPreprocessor.php
@@ -6,6 +6,7 @@ use Drupal;
 use Drupal\Core\Url;
 use Drupal\tofu\Constant;
 use Drupal\activeforanimals\Controller\ProfileBarController;
+use Drupal\effective_activism\Controller\Element\ButtonController;
 use Drupal\effective_activism\Controller\Element\ElementController;
 use Drupal\effective_activism\Controller\Element\ImageController;
 use Drupal\user\Form\UserLoginForm;
@@ -22,6 +23,7 @@ class HeaderMenuPreprocessor extends Preprocessor implements PreprocessorInterfa
     // Fetch elements.
     $element_controller = new ElementController();
     $image_controller = new ImageController();
+    $button_controller = new ButtonController();
     $logo_path = sprintf('%s/images/logo.png', drupal_get_path('theme', Constant::MACHINE_NAME));
     $front_page_link = new Url('activeforanimals.frontpage');
     $organization_overview_link = new Url('activeforanimals.organization.overview');
@@ -29,7 +31,7 @@ class HeaderMenuPreprocessor extends Preprocessor implements PreprocessorInterfa
     $this->variables['content']['guide'] = $element_controller->view(t('Guide'), 'guide');
     $this->variables['content']['organizations'] = $element_controller->view(t('Organizations'), 'organizations', $organization_overview_link);
     $this->variables['content']['help'] = $element_controller->view(t('Help'), 'help');
-    $this->variables['content']['login'] = Drupal::formBuilder()->getForm(UserLoginForm::class);
+    $this->variables['content']['login'] = $button_controller->view(t('Log in'), 'login', new Url('user.login'));
     $profile_bar_controller = new ProfileBarController();
     $this->variables['content']['profile_bar'] = $profile_bar_controller->content();
     return $this->variables;
