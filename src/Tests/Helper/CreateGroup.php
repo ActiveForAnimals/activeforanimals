e5b43d1dc0301de982389065714057df3c716ec8
Peter Rønsby <pr@activeforanimals.com>
2017-10-16 21:02:30 +0200

    AFA-124 Add default result types to CreateGroup helper class

--- src/Tests/Helper/CreateGroup.php
+++ src/Tests/Helper/CreateGroup.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\activeforanimals\Tests\Helper;
 
+use Drupal\effective_activism\Constant;
 use Drupal\effective_activism\Entity\Group;
 use Drupal\effective_activism\Entity\Organization;
+use Drupal\effective_activism\Helper\ResultTypeHelper;
 use Drupal\user\Entity\User;
 
 /**
@@ -29,11 +31,14 @@ class CreateGroup {
    *   The organizer of the group.
    * @param string $title
    *   Optional title of the group.
+   * @param bool $use_default_result_types
+   *   If TRUE, adds this group to the organization default result types.
    */
-  public function __construct(Organization $organization, User $organizer, $title = NULL) {
+  public function __construct(Organization $organization, User $organizer, $title = NULL, $use_default_result_types = FALSE) {
     $this->organization = $organization;
     $this->organizer = $organizer;
     $this->title = empty($title) ? self::TITLE : $title;
+    $this->use_default_result_types = $use_default_result_types;
   }
 
   /**
@@ -48,6 +53,14 @@ class CreateGroup {
       'organization' => $this->organization->id(),
     ]);
     $group->save();
+    // Activate all default result types for this group.
+    if ($this->use_default_result_types === TRUE) {
+      foreach (Constant::DEFAULT_RESULT_TYPES as $import_name => $settings) {
+        $result_type = ResultTypeHelper::getResultTypeByImportName($import_name, $this->organization->id());
+        $result_type->groups = array_merge($result_type->groups, [$group->id() => $group->id()]);
+      }
+      $result_type->save();
+    }
     return $group;
   }
 
