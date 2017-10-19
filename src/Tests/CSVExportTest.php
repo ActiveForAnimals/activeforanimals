508ae76d01b6b06c5a390bc8eeb2afe0de0d3556
Peter Rønsby <pr@activeforanimals.com>
2017-10-19 13:38:25 +0200

    AFA-124 Updated publish test and added export test

---
+++ src/Tests/CSVExportTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Drupal\activeforanimals\Tests;
+
+use Drupal;
+use Drupal\activeforanimals\Tests\Helper\CreateOrganization;
+use Drupal\effective_activism\Helper\GroupHelper;
+use Drupal\effective_activism\Helper\OrganizationHelper;
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Function tests for creating CSV exports.
+ *
+ * @group activeforanimals
+ */
+class CSVExportTest extends WebTestBase {
+
+  const ADD_CSV_EXPORT_PATH = '/export/csv';
+  const RESULTTYPE = 'leafleting';
+  const RESULT = 'leafleting | 4 | 0 | 1 | 0 | 1000 | Flyer A';
+  const STARTDATE = '12/13/2016';
+  const STARTTIME = '11:00';
+  const ENDDATE = '12/13/2016';
+  const ENDTIME = '13:00';
+  const NUMBER_OF_EXPORTED_EVENTS = 2;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $profile = 'activeforanimals';
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'effective_activism',
+  ];
+
+  /**
+   * The test organization.
+   *
+   * @var Organization
+   */
+  private $organization;
+
+  /**
+   * The test group.
+   *
+   * @var Group
+   */
+  private $group;
+
+  /**
+   * The test manager.
+   *
+   * @var User
+   */
+  private $manager;
+
+  /**
+   * The test organizer.
+   *
+   * @var User
+   */
+  private $organizer;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+    // Disable user timezones.
+    // This is required in order for events to register correct time.
+    $systemDate = Drupal::configFactory()->getEditable('system.date');
+    $systemDate->set('timezone.default', 'UTC');
+    $systemDate->save(TRUE);
+    // Create users and structure.
+    $this->manager = $this->drupalCreateUser();
+    $this->organizer = $this->drupalCreateUser();
+    $this->organization = (new CreateOrganization($this->manager, $this->organizer))->execute();
+    $groups = OrganizationHelper::getGroups($this->organization);
+    $this->group = array_pop($groups);
+  }
+
+  /**
+   * Run test.
+   */
+  public function testDo() {
+    // Export CSV file.
+    $this->drupalLogin($this->organizer);
+    $this->drupalGet(self::ADD_CSV_EXPORT_PATH);
+    $this->assertResponse(200);
+    $this->drupalPostForm(NULL, [
+      'parent[0][target_id]' => $this->group->id(),
+    ], t('Save'));
+    $this->assertResponse(200);
+    $this->assertText('Created the export.', 'Added a new export entity.');
+    $this->assertText(sprintf('%d items exported.', self::NUMBER_OF_EXPORTED_EVENTS), 'Successfully exported events');
+    
+  }
+
+}
--- src/Tests/PublishTest.php
+++ src/Tests/PublishTest.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\activeforanimals\Tests;
 
-use Drupal\activeforanimals\Tests\Helper\CreateOrganization;
-use Drupal\activeforanimals\Tests\Helper\CreateGroup;
 use Drupal\activeforanimals\Tests\Helper\CreateEvent;
+use Drupal\activeforanimals\Tests\Helper\CreateGroup;
+use Drupal\activeforanimals\Tests\Helper\CreateImport;
+use Drupal\activeforanimals\Tests\Helper\CreateExport;
+use Drupal\activeforanimals\Tests\Helper\CreateOrganization;
 use Drupal\effective_activism\Entity\Group;
 use Drupal\effective_activism\Entity\Organization;
 use Drupal\simpletest\WebTestBase;
@@ -18,6 +20,7 @@ class PublishTest extends WebTestBase {
 
   const PATH_EVENT_ADD = 'create-event';
   const ADD_CSV_IMPORT_PATH = 'import/csv';
+  const ADD_CSV_EXPORT_PATH = 'export/csv';
   const GROUP_TITLE_1 = 'Test group 1';
   const GROUP_TITLE_1_MODIFIED = 'Test group 1 (updated)';
   const GROUP_TITLE_2 = 'Test group 2';
@@ -75,6 +78,13 @@ class PublishTest extends WebTestBase {
   private $organizer;
 
   /**
+   * The path to the CSV test file.
+   *
+   * @var string
+   */
+  private $csvfilepath;
+
+  /**
    * {@inheritdoc}
    */
   public function setUp() {
@@ -83,8 +93,11 @@ class PublishTest extends WebTestBase {
     $this->organizer = $this->drupalCreateUser();
     // Create organizational structure.
     $this->organization = (new CreateOrganization($this->manager, $this->organizer))->execute();
-    $this->group = (new CreateGroup($this->organization, $this->organizer))->execute();
+    $this->group = (new CreateGroup($this->organization, $this->organizer, NULL, TRUE))->execute();
     $this->event = (new CreateEvent($this->group, $this->organizer))->execute();
+    $this->csvfilepath = $this->container->get('file_system')->realpath(drupal_get_path('profile', 'activeforanimals') . '/src/Tests/testdata/sample.csv');
+    $this->import = (new CreateImport($this->group, $this->organizer, $this->csvfilepath))->execute();
+    $this->export = (new CreateExport($this->group, $this->organizer))->execute();
   }
 
   /**
@@ -153,11 +166,11 @@ class PublishTest extends WebTestBase {
     $this->assertText('2 items published.');
     $this->drupalGet(sprintf('%s/publish', $this->organization->toUrl()->toString()));
     $this->drupalPostForm(NULL, [], t('Unpublish'));
-    $this->assertText('4 items unpublished.');
+    $this->assertText('10 items unpublished.');
 
     // Verify that organizer cannot access organization, group and event.
     $this->drupalLogin($this->organizer);
-    // User has access to organization page.
+    // User does not have access to organization page.
     $this->drupalGet($this->organization->toUrl()->toString());
     $this->assertResponse(403);
     // User does not have access to group page.
@@ -166,6 +179,60 @@ class PublishTest extends WebTestBase {
     // User does not have access to event page.
     $this->drupalGet($this->event->toUrl()->toString());
     $this->assertResponse(403);
+
+    // Unpublish import and events.
+    $this->drupalLogin($this->manager);
+    $this->drupalGet(sprintf('%s/publish', $this->import->toUrl()->toString()));
+    $this->drupalPostForm(NULL, [], t('Unpublish'));
+    $this->assertText('6 items unpublished.');
+
+    // Verify that organizer cannot access import and events.
+    $this->drupalLogin($this->organizer);
+    // User does not have access to import page.
+    $this->drupalGet($this->import->toUrl()->toString());
+    $this->assertResponse(403);
+    // User does not have access to event page.
+    $this->drupalGet(sprintf('%s/e/2', $this->group->toUrl()->toString()));
+    $this->assertResponse(403);
+
+    // Publish import and events.
+    $this->drupalLogin($this->manager);
+    $this->drupalGet(sprintf('%s/publish', $this->import->toUrl()->toString()));
+    $this->drupalPostForm(NULL, [], t('Publish'));
+    $this->assertText('6 items published.');
+
+    // Verify that organizer can access import and events.
+    $this->drupalLogin($this->organizer);
+    // User has access to import page.
+    $this->drupalGet($this->import->toUrl()->toString());
+    $this->assertResponse(200);
+    // User has access to event page.
+    $this->drupalGet(sprintf('%s/e/2', $this->group->toUrl()->toString()));
+    $this->assertResponse(200);
+
+    // Unpublish export.
+    $this->drupalLogin($this->manager);
+    $this->drupalGet(sprintf('%s/publish', $this->export->toUrl()->toString()));
+    $this->drupalPostForm(NULL, [], t('Unpublish'));
+    $this->assertText('One item unpublished.');
+
+    // Verify that organizer cannot access export.
+    $this->drupalLogin($this->organizer);
+    // User does not have access to export page.
+    $this->drupalGet($this->export->toUrl()->toString());
+    $this->assertResponse(403);
+
+    // Publish import and events.
+    $this->drupalLogin($this->manager);
+    $this->drupalGet(sprintf('%s/publish', $this->export->toUrl()->toString()));
+    $this->drupalPostForm(NULL, [], t('Publish'));
+    $this->assertText('One item published.');
+
+    // Verify that organizer can access import and events.
+    $this->drupalLogin($this->organizer);
+    // User has access to import page.
+    $this->drupalGet($this->export->toUrl()->toString());
+    $this->assertResponse(200);
   }
 
 }
