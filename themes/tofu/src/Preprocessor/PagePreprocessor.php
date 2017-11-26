1bee14c54ba76ff388fa5bdfe083bd460824ceb7
Peter Rønsby <pr@activeforanimals.com>
2017-11-26 10:43:06 +0100

    AFA-150 Unrelated: Added newsletter to page footer and own page

--- themes/tofu/src/Preprocessor/PagePreprocessor.php
+++ themes/tofu/src/Preprocessor/PagePreprocessor.php
@@ -24,11 +24,16 @@ class PagePreprocessor extends Preprocessor implements PreprocessorInterface {
     $this->variables['invitations'] = (new InvitationController())->view();
     // Add external site links.
     $facebook_logo_path = sprintf('%s/images/facebook.png', drupal_get_path('theme', Constant::MACHINE_NAME));
+    $mailchimp_logo_path = sprintf('%s/images/mailchimp.png', drupal_get_path('theme', Constant::MACHINE_NAME));
     $github_logo_path = sprintf('%s/images/github.png', drupal_get_path('theme', Constant::MACHINE_NAME));
     $this->variables['footer']['facebook_logo'] = $image_controller->view($facebook_logo_path, 'facebook_logo', NULL, Url::fromUri('https://www.facebook.com/activeforanimals'));
     $this->variables['footer']['facebook_link'] = $element_controller->view(t('Facebook'), 'facebook_link', Url::fromUri('https://www.facebook.com/activeforanimals'));
+    $this->variables['footer']['mailchimp_logo'] = $image_controller->view($mailchimp_logo_path, 'mailchimp_logo', NULL, Url::fromRoute('activeforanimals.newsletter.sign_up'));
+    $this->variables['footer']['mailchimp_link'] = $element_controller->view(t('Newsletter sign up'), 'mailchimp_link', Url::fromRoute('activeforanimals.newsletter.sign_up'));
     $this->variables['footer']['email_address'] = $element_controller->view('info (at) activeforanimals.com', 'email', Url::fromUri('mailto: info (at) activeforanimals.com'));
     $this->variables['footer']['feedback'] = $element_controller->view('Give us feedback', 'link', Url::fromRoute('contact.site_page'));
+    $this->variables['footer']['afa_message'] = $element_controller->view('Active for Animals 2016', 'link', Url::fromRoute('activeforanimals.frontpage'));
+    $this->variables['footer']['darksky_message'] = $element_controller->view('Powered by Dark Sky', 'link', Url::fromUri('https://darksky.net/poweredby/'));
     // Add theme path to drupalSettings.
     $this->variables['#attached']['drupalSettings'] = [
       'tofu' => [
