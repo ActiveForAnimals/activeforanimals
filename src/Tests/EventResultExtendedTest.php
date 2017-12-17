a4c2ea7dffb18cd4c17bc9c67383a3295a8946c2
Peter Rønsby <pr@activeforanimals.com>
2017-12-17 10:49:22 +0100

    AFA-183 Code style fixes

--- src/Tests/EventResultExtendedTest.php
+++ src/Tests/EventResultExtendedTest.php
@@ -2,13 +2,10 @@
 
 namespace Drupal\activeforanimals\Tests;
 
-use Drupal;
-use Drupal\activeforanimals\Tests\Helper\CreateEvent;
 use Drupal\activeforanimals\Tests\Helper\CreateGroup;
 use Drupal\activeforanimals\Tests\Helper\CreateOrganization;
 use Drupal\effective_activism\Entity\Group;
 use Drupal\effective_activism\Helper\ResultTypeHelper;
-use Drupal\effective_activism\Constant;
 use Drupal\simpletest\WebTestBase;
 
 /**
