8294365e17d03561384bb25b243ef9cee1aa7c36
Peter Rønsby <pr@activeforanimals.com>
2018-02-04 20:25:38 +0200

    AFA-201 Refined group access test and added organizer check to group test

--- src/Tests/AccessRestrictionsTest.php
+++ src/Tests/AccessRestrictionsTest.php
@@ -36,6 +36,9 @@ class AccessRestrictionsTest extends WebTestBase {
   const STARTTIME = '11:00';
   const ENDDATE = '2016-01-01';
   const ENDTIME = '12:00';
+  const PATTERN_ORGANIZATION_SELECTOR = '"edit-organization-wrapper"';
+  const PATTERN_ORGANIZER_SECTION = '"edit-organizers-wrapper"';
+  const PATTERN_RESULT_TYPE_SECTION = '"element-result_types"';
 
   /**
    * {@inheritdoc}
@@ -227,11 +230,18 @@ class AccessRestrictionsTest extends WebTestBase {
     $this->assertResponse(403);
     $this->drupalGet(sprintf('%s/edit', $this->group1->toUrl()->toString()));
     $this->assertResponse(200);
-    $this->assertNoText('Result types', 'Result type section not available');
-    $this->assertNoText('Invite new organizer', 'Organizer section not available');
+    $this->assertNoPattern(self::PATTERN_RESULT_TYPE_SECTION, 'Result type section not available');
+    $this->assertNoPattern(self::PATTERN_ORGANIZER_SECTION, 'Organizer section not available');
+    $this->assertNoPattern(self::PATTERN_ORGANIZATION_SELECTOR, 'Organization selection not available');
 
-    // Verify that manager1 can create events for group1.
+    // Verify that manager1 can access restricted group fields.
     $this->drupalLogin($this->manager1);
+    $this->drupalGet(sprintf('%s/edit', $this->group1->toUrl()->toString()));
+    $this->assertResponse(200);
+    $this->assertPattern(self::PATTERN_RESULT_TYPE_SECTION, 'Result type section available');
+    $this->assertPattern(self::PATTERN_ORGANIZER_SECTION, 'Organizer section available');
+    $this->assertPattern(self::PATTERN_ORGANIZATION_SELECTOR, 'Organization selection available');
+    // Verify that manager1 can create events for group1.
     // User has access to event overview page.
     $this->drupalGet(sprintf('%s/e', $this->group1->toUrl()->toString()));
     $this->assertResponse(200);
