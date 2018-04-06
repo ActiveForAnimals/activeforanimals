937c0c27d2ca76b8db148cf51757db1e1c17ed5c
Peter Rønsby <pr@activeforanimals.com>
2018-04-06 14:48:52 +0200

    AFA-225 Remove placeholder text

--- themes/tofu/src/Preprocessor/NewsletterSignUpFormPreprocessor.php
+++ themes/tofu/src/Preprocessor/NewsletterSignUpFormPreprocessor.php
@@ -14,7 +14,6 @@ class NewsletterSignUpFormPreprocessor extends Preprocessor implements Preproces
     $this->variables['form']['email'] = $this->wrapFormElement($this->variables['form']['email'], 'email');
     $this->variables['form']['first_name'] = $this->wrapFormElement($this->variables['form']['first_name'], 'name');
     $this->variables['form']['last_name'] = $this->wrapFormElement($this->variables['form']['last_name'], 'name');
-    $this->variables['content']['latest_news'] = 'xxx';
     return $this->variables;
   }
 
