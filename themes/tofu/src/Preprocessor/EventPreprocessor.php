d764531411e194d559e9c5e3aee32ee6b0f5b7dd
Peter Rønsby <pr@activeforanimals.com>
2018-09-03 15:46:10 +0200

    AFA-258 Added theming support for conditionally displaying third-party content on event pages

--- themes/tofu/src/Preprocessor/EventPreprocessor.php
+++ themes/tofu/src/Preprocessor/EventPreprocessor.php
@@ -139,59 +139,60 @@ class EventPreprocessor extends Preprocessor implements PreprocessorInterface {
       }
       if (!$event->third_party_content->isEmpty()) {
         foreach ($event->third_party_content as $item) {
-          switch ($item->entity->getType()) {
-            case EffectiveActivismConstant::THIRD_PARTY_CONTENT_TYPE_WEATHER_INFORMATION:
-              $this->variables['weather_information']['source'] = $item->entity->get('source')->isEmpty() ? NULL : $this->wrapField($item->entity->get('source'));
-              $this->variables['content']['weather_information']['temperature'] = $item->entity->get('field_temperature')->isEmpty() ? NULL : $this->wrapElement(t('Temperature: @temperature °C', ['@temperature' => round($item->entity->get('field_temperature')->value)]), 'temperature');
-              $this->variables['content']['weather_information']['precipitation'] = ($item->entity->get('field_precipitation_type')->isEmpty() || $item->entity->get('field_precipitation_intensity')->isEmpty()) ? NULL : $this->wrapElement(t('Precipitation: @precipitation_intensity mm/h @precipitation_type', [
-                '@precipitation_type' => $item->entity->get('field_precipitation_type')->value,
-                '@precipitation_intensity' => $item->entity->get('field_precipitation_intensity')->value,
-              ]), 'precipitation');
-              $this->variables['content']['weather_information']['visibility'] = $item->entity->get('field_visibility')->isEmpty() ? NULL : $this->wrapElement(t('Visibility: @visibility km', ['@visibility' => $item->entity->get('field_visibility')->value]), 'visibility');
-              $this->variables['content']['weather_information']['windspeed'] = $item->entity->get('field_windspeed')->isEmpty() ? NULL : $this->wrapElement(t('Windspeed: @windspeed m/s', ['@windspeed' => $item->entity->get('field_windspeed')->value]), 'windspeed');
-              break;
+          if ($item->entity->isPublished()) {
+            switch ($item->entity->getType()) {
+              case EffectiveActivismConstant::THIRD_PARTY_CONTENT_TYPE_WEATHER_INFORMATION:
+                $this->variables['weather_information']['source'] = $item->entity->get('source')->isEmpty() ? NULL : $this->wrapField($item->entity->get('source'));
+                $this->variables['content']['weather_information']['temperature'] = $item->entity->get('field_temperature')->isEmpty() ? NULL : $this->wrapElement(t('Temperature: @temperature °C', ['@temperature' => round($item->entity->get('field_temperature')->value)]), 'temperature');
+                $this->variables['content']['weather_information']['precipitation'] = ($item->entity->get('field_precipitation_type')->isEmpty() || $item->entity->get('field_precipitation_intensity')->isEmpty()) ? NULL : $this->wrapElement(t('Precipitation: @precipitation_intensity mm/h @precipitation_type', [
+                  '@precipitation_type' => $item->entity->get('field_precipitation_type')->value,
+                  '@precipitation_intensity' => $item->entity->get('field_precipitation_intensity')->value,
+                ]), 'precipitation');
+                $this->variables['content']['weather_information']['visibility'] = $item->entity->get('field_visibility')->isEmpty() ? NULL : $this->wrapElement(t('Visibility: @visibility km', ['@visibility' => $item->entity->get('field_visibility')->value]), 'visibility');
+                $this->variables['content']['weather_information']['windspeed'] = $item->entity->get('field_windspeed')->isEmpty() ? NULL : $this->wrapElement(t('Windspeed: @windspeed m/s', ['@windspeed' => $item->entity->get('field_windspeed')->value]), 'windspeed');
+                break;
 
-            case EffectiveActivismConstant::THIRD_PARTY_CONTENT_TYPE_DEMOGRAPHICS:
-              $this->variables['demographics']['source'] = $item->entity->get('source')->isEmpty() ? NULL : $this->wrapField($item->entity->get('source'));
-              $this->variables['content']['demographics']['total_population'] = $item->entity->get('field_total_population')->isEmpty() ? NULL : $this->wrapElement(t('Total population: @total_population', ['@total_population' => $item->entity->get('field_total_population')->value]), 'total_population');
-              $this->variables['content']['demographics']['male_population'] = $item->entity->get('field_male_population')->isEmpty() ? NULL : $this->wrapElement(t('Male population: @male_population', ['@male_population' => $item->entity->get('field_male_population')->value]), 'male_population');
-              $this->variables['content']['demographics']['female_population'] = $item->entity->get('field_female_population')->isEmpty() ? NULL : $this->wrapElement(t('Female population: @female_population', ['@female_population' => $item->entity->get('field_female_population')->value]), 'female_population');
-              $this->variables['content']['demographics']['total_households'] = $item->entity->get('field_total_households')->isEmpty() ? NULL : $this->wrapElement(t('Total households: @total_households', ['@total_households' => $item->entity->get('field_total_households')->value]), 'total_households');
-              $this->variables['content']['demographics']['average_household_size'] = $item->entity->get('field_average_household_size')->isEmpty() ? NULL : $this->wrapElement(t('Average household: @average_household_size', ['@average_household_size' => $item->entity->get('field_average_household_size')->value]), 'average_household_size');
-              break;
+              case EffectiveActivismConstant::THIRD_PARTY_CONTENT_TYPE_DEMOGRAPHICS:
+                $this->variables['demographics']['source'] = $item->entity->get('source')->isEmpty() ? NULL : $this->wrapField($item->entity->get('source'));
+                $this->variables['content']['demographics']['total_population'] = $item->entity->get('field_total_population')->isEmpty() ? NULL : $this->wrapElement(t('Total population: @total_population', ['@total_population' => $item->entity->get('field_total_population')->value]), 'total_population');
+                $this->variables['content']['demographics']['male_population'] = $item->entity->get('field_male_population')->isEmpty() ? NULL : $this->wrapElement(t('Male population: @male_population', ['@male_population' => $item->entity->get('field_male_population')->value]), 'male_population');
+                $this->variables['content']['demographics']['female_population'] = $item->entity->get('field_female_population')->isEmpty() ? NULL : $this->wrapElement(t('Female population: @female_population', ['@female_population' => $item->entity->get('field_female_population')->value]), 'female_population');
+                $this->variables['content']['demographics']['total_households'] = $item->entity->get('field_total_households')->isEmpty() ? NULL : $this->wrapElement(t('Total households: @total_households', ['@total_households' => $item->entity->get('field_total_households')->value]), 'total_households');
+                $this->variables['content']['demographics']['average_household_size'] = $item->entity->get('field_average_household_size')->isEmpty() ? NULL : $this->wrapElement(t('Average household: @average_household_size', ['@average_household_size' => $item->entity->get('field_average_household_size')->value]), 'average_household_size');
+                break;
 
-            case EffectiveActivismConstant::THIRD_PARTY_CONTENT_TYPE_EXTENDED_LOCATION_INFORMATION:
-              $this->variables['extended_location_information']['source'] = $item->entity->get('source')->isEmpty() ? NULL : $this->wrapField($item->entity->get('source'));
-              $this->variables['content']['extended_location_information']['gps_coordinates'] = $item->entity->get('field_latitude')->isEmpty() ? NULL : $this->wrapElement(t('GPS coordinates: @latitude, @longitude', [
-                '@latitude' => substr($item->entity->get('field_latitude')->value, 0, 10),
-                '@longitude' => substr($item->entity->get('field_longitude')->value, 0, 10),
-              ]), 'gps_coordinates');
-              $this->variables['content']['extended_location_information']['address_locality'] = $item->entity->get('field_address_locality')->isEmpty() ? NULL : $this->wrapElement(t('Locality: @address_locality', ['@address_locality' => $item->entity->get('field_address_locality')->value]), 'address_locality');
-              $this->variables['content']['extended_location_information']['address_sublocality'] = $item->entity->get('field_address_sublocality')->isEmpty() ? NULL : $this->wrapElement(t('Sublocality: @address_sublocality', ['@address_sublocality' => $item->entity->get('field_address_sublocality')->value]), 'address_sublocality');
-              $this->variables['content']['extended_location_information']['address_neighborhood'] = $item->entity->get('field_address_neighborhood')->isEmpty() ? NULL : $this->wrapElement(t('Neighborhood: @address_neighborhood', ['@address_neighborhood' => $item->entity->get('field_address_neighborhood')->value]), 'address_neighborhood');
-              $this->variables['content']['extended_location_information']['address_postal_code'] = $item->entity->get('field_address_postal_code')->isEmpty() ? NULL : $this->wrapElement(t('Postal code: @address_postal_code', ['@address_postal_code' => $item->entity->get('field_address_postal_code')->value]), 'address_postal_code');
-              break;
+              case EffectiveActivismConstant::THIRD_PARTY_CONTENT_TYPE_EXTENDED_LOCATION_INFORMATION:
+                $this->variables['extended_location_information']['source'] = $item->entity->get('source')->isEmpty() ? NULL : $this->wrapField($item->entity->get('source'));
+                $this->variables['content']['extended_location_information']['gps_coordinates'] = $item->entity->get('field_latitude')->isEmpty() ? NULL : $this->wrapElement(t('GPS coordinates: @latitude, @longitude', [
+                  '@latitude' => substr($item->entity->get('field_latitude')->value, 0, 10),
+                  '@longitude' => substr($item->entity->get('field_longitude')->value, 0, 10),
+                ]), 'gps_coordinates');
+                $this->variables['content']['extended_location_information']['address_locality'] = $item->entity->get('field_address_locality')->isEmpty() ? NULL : $this->wrapElement(t('Locality: @address_locality', ['@address_locality' => $item->entity->get('field_address_locality')->value]), 'address_locality');
+                $this->variables['content']['extended_location_information']['address_sublocality'] = $item->entity->get('field_address_sublocality')->isEmpty() ? NULL : $this->wrapElement(t('Sublocality: @address_sublocality', ['@address_sublocality' => $item->entity->get('field_address_sublocality')->value]), 'address_sublocality');
+                $this->variables['content']['extended_location_information']['address_neighborhood'] = $item->entity->get('field_address_neighborhood')->isEmpty() ? NULL : $this->wrapElement(t('Neighborhood: @address_neighborhood', ['@address_neighborhood' => $item->entity->get('field_address_neighborhood')->value]), 'address_neighborhood');
+                $this->variables['content']['extended_location_information']['address_postal_code'] = $item->entity->get('field_address_postal_code')->isEmpty() ? NULL : $this->wrapElement(t('Postal code: @address_postal_code', ['@address_postal_code' => $item->entity->get('field_address_postal_code')->value]), 'address_postal_code');
+                break;
 
-            case EffectiveActivismConstant::THIRD_PARTY_CONTENT_TYPE_CITY_PULSE:
-              $this->variables['city_pulse']['source'] = $item->entity->get('source')->isEmpty() ? NULL : $this->wrapField($item->entity->get('source'));
-              $field_goldenstein_scale_allowed_values = $item->entity->get('field_goldenstein_scale')->getItemDefinition()->getSetting('allowed_values');
-              $this->variables['content']['city_pulse']['goldenstein_scale'] = $item->entity->get('field_goldenstein_scale')->isEmpty() ? NULL : $this->wrapElement(t('Goldenstein scale: @goldenstein_scale', ['@goldenstein_scale' => $field_goldenstein_scale_allowed_values[$item->entity->get('field_goldenstein_scale')->value]]), 'goldenstein_scale');
-              $field_tone_allowed_values = $item->entity->get('field_tone')->getItemDefinition()->getSetting('allowed_values');
-              $this->variables['content']['city_pulse']['tone'] = $item->entity->get('field_tone')->isEmpty() ? NULL : $this->wrapElement(t('Average tone: @tone', ['@tone' => $field_tone_allowed_values[$item->entity->get('field_tone')->value]]), 'tone');
-              if (!$item->entity->get('field_news_sources')->isEmpty()) {
-                $sources = '';
-                $i = 1;
-                foreach ($item->entity->get('field_news_sources') as $source) {
-                  $sources .= sprintf('<a href="%s" target="_blank">%d</a> ', $source->getString(), $i);
-                  $i++;
+              case EffectiveActivismConstant::THIRD_PARTY_CONTENT_TYPE_CITY_PULSE:
+                $this->variables['city_pulse']['source'] = $item->entity->get('source')->isEmpty() ? NULL : $this->wrapField($item->entity->get('source'));
+                $field_goldenstein_scale_allowed_values = $item->entity->get('field_goldenstein_scale')->getItemDefinition()->getSetting('allowed_values');
+                $this->variables['content']['city_pulse']['goldenstein_scale'] = $item->entity->get('field_goldenstein_scale')->isEmpty() ? NULL : $this->wrapElement(t('Goldenstein scale: @goldenstein_scale', ['@goldenstein_scale' => $field_goldenstein_scale_allowed_values[$item->entity->get('field_goldenstein_scale')->value]]), 'goldenstein_scale');
+                $field_tone_allowed_values = $item->entity->get('field_tone')->getItemDefinition()->getSetting('allowed_values');
+                $this->variables['content']['city_pulse']['tone'] = $item->entity->get('field_tone')->isEmpty() ? NULL : $this->wrapElement(t('Average tone: @tone', ['@tone' => $field_tone_allowed_values[$item->entity->get('field_tone')->value]]), 'tone');
+                if (!$item->entity->get('field_news_sources')->isEmpty()) {
+                  $sources = '';
+                  $i = 1;
+                  foreach ($item->entity->get('field_news_sources') as $source) {
+                    $sources .= sprintf('<a href="%s" target="_blank">%d</a> ', $source->getString(), $i);
+                    $i++;
+                  }
+                  $formatted_sources = new FormattableMarkup($sources, []);
+                  $this->variables['content']['city_pulse']['news_sources'] = $this->wrapElement(t('News sources: @sources', ['@sources' => $formatted_sources]), 'news_sources');
+                } else {
+                  $this->variables['content']['city_pulse']['news_sources'] = NULL;
                 }
-                $formatted_sources = new FormattableMarkup($sources, []);
-                $this->variables['content']['city_pulse']['news_sources'] = $this->wrapElement(t('News sources: @sources', ['@sources' => $formatted_sources]), 'news_sources');
-              }
-              else {
-                $this->variables['content']['city_pulse']['news_sources'] = NULL;
-              }
-              break;
+                break;
+            }
           }
         }
       }
